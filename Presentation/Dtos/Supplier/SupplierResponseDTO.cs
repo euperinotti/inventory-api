5034e5f6be2b5edc4856abad9caf9495d7bf1286
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-12-01 16:38:16 -0300

    feat: add SupplierResponseDTO.cs

---
+++ Presentation/Dtos/Supplier/SupplierResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace inventory_api.Presentation.Dtos.Supplier;
+
+public class SupplierResponseDTO
+{
+    public long? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Cnpj { get; set; }
+    public string? Address { get; set; }
+    public string? Contact { get; set; }
+}
