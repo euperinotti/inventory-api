5809c2ec06b4ae4b2a2493fb959faf1d30ba7ab2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-12-01 15:44:39 -0300

    feat: add PgsqlSupplierEntity.cs

---
+++ Infra/Pgsql/Entities/PgsqlSupplierEntity.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace inventory_api.Infra.Pgsql.Entities;
+
+[Table("suppliers")]
+public class PgsqlSupplierEntity
+{
+    [Key] public long? id { get; private set; }
+    public string name { get; private set; }
+    public string cnpj { get; private set; }
+    public string contact { get; private set; }
+    public string address { get; private set; }
+
+    public PgsqlSupplierEntity(long? id, string name, string cnpj, string contact, string address)
+    {
+        this.id = id;
+        this.name = name;
+        this.cnpj = cnpj;
+        this.contact = contact;
+        this.address = address;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Supplier name is required");
+        }
+
+        if (string.IsNullOrWhiteSpace(cnpj))
+        {
+            throw new ArgumentException("Supplier CNPJ is required");
+        }
+    }
+}
