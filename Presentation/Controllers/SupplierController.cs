8d072e49a0875f73131c3a3372b57f06a23f1eae
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-12-01 16:38:25 -0300

    feat: add SupplierController.cs

---
+++ Presentation/Controllers/SupplierController.cs
@@ -0,0 +1,31 @@
+﻿using inventory_api.Domain.Repository;
+using inventory_api.Presentation.Dtos.Supplier;
+using Microsoft.AspNetCore.Mvc;
+
+namespace inventory_api.Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SupplierController : Controller
+{
+    private readonly ILogger<SupplierController> _logger;
+    private readonly ISupplierRepository _repository;
+
+    [HttpGet]
+    [Route("/")]
+    public ActionResult<IEnumerator<SupplierResponseDTO>> GetAll()
+    {
+        // List<SupplierResponseDTO> response = _repository.FindAll();
+
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("/ping")]
+    public ActionResult<IEnumerator<SupplierResponseDTO>> Ping()
+    {
+        // List<SupplierResponseDTO> response = _repository.FindAll();
+
+        return Ok(new { Pong = true });
+    }
+}
