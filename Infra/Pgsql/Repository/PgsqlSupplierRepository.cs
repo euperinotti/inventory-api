2394ded975c41a3559eba4c086a52862ad7cf60b
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-12-01 16:39:17 -0300

    feat: add supplier repository implementation

---
+++ Infra/Pgsql/Repository/PgsqlSupplierRepository.cs
@@ -0,0 +1,37 @@
+﻿using inventory_api.Domain.Entities;
+using inventory_api.Domain.Repository;
+
+namespace inventory_api.Infra.Pgsql.Repository;
+
+public class PgsqlSupplierRepository : ISupplierRepository
+{
+    public IEnumerable<SupplierBO> FindAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public SupplierBO? FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public SupplierBO Create(SupplierBO supplier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public SupplierBO? Update(SupplierBO supplier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(SupplierBO supplier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int? id)
+    {
+        throw new NotImplementedException();
+    }
+}
