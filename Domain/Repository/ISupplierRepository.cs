69f96224081cd23f488f22aa88dd6fc9e477d34e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-12-01 16:38:37 -0300

    feat: add ISupplierRepository.cs

---
+++ Domain/Repository/ISupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using inventory_api.Domain.Entities;
+
+namespace inventory_api.Domain.Repository;
+
+public interface ISupplierRepository
+{
+    public IEnumerable<SupplierBO> FindAll();
+    public SupplierBO? FindById(int id);
+    public SupplierBO Create(SupplierBO supplier);
+    public SupplierBO? Update(SupplierBO supplier);
+    public void Delete(SupplierBO supplier);
+    public void Delete(int? id);
+}
