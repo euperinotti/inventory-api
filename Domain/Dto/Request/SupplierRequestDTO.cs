238b6b8c425e9f104e3184705352c16d2e82ac5b
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-23 00:07:52 -0300

    feat: add supplier request dto

---
+++ Domain/Dto/Request/SupplierRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace inventory_api.Domain.Dto.Request;
+
+public record SupplierRequestDTO
+{
+    public long? Id { get; set; }
+    public string Name { get; set; }
+    public string Cnpj { get; set; }
+    public string Address { get; set; }
+    public string Contact { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
