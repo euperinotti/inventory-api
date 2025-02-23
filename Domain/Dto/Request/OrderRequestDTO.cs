149fc172855692fe65f028189bb960abcb399d6e
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-23 00:06:38 -0300

    feat: add order request dto

---
+++ Domain/Dto/Request/OrderRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace inventory_api.Domain.Dto.Request;
+
+public record OrderRequestDTO
+{
+    public long? Id { get; set; }
+    public DateTime Date { get; set; }
+    public OrderStatus Status { get; set; }
+    public long SupplierId { get; set; }
+    public decimal Total { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
