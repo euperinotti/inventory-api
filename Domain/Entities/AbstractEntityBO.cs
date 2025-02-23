af838ae44769b28d3d28d0e685f86400c32e6f9b
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-22 23:56:11 -0300

    feat: add generic entity bo

---
+++ Domain/Entities/AbstractEntityBO.cs
@@ -0,0 +1,15 @@
+﻿namespace inventory_api.Domain.Entities;
+
+public abstract class AbstractEntityBO
+{
+    public long? Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    protected AbstractEntityBO(long? id, DateTime createdAt, DateTime updatedAt)
+    {
+        Id = id;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+}
