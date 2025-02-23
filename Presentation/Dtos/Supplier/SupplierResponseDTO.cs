fe2dee01cf6f4870e87635ac0032601fc6403bce
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-23 13:15:30 -0300

    refactor: add missing fields to SupplierResponseDTO.cs

--- Presentation/Dtos/Supplier/SupplierResponseDTO.cs
+++ Presentation/Dtos/Supplier/SupplierResponseDTO.cs
@@ -7,4 +7,6 @@ public class SupplierResponseDTO
     public string? Cnpj { get; set; }
     public string? Address { get; set; }
     public string? Contact { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
