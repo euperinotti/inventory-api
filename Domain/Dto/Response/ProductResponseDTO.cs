9e283154e37dcb42f3e5cf69ad5dcfb2f6889509
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-23 13:14:19 -0300

    feat: add ProductResponseDTO.cs

---
+++ Domain/Dto/Response/ProductResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using InventoryApi.Presentation.Dtos.Supplier;
+
+namespace InventoryApi.Domain.Dto.Response;
+
+public record ProductResponseDTO
+{
+    public long? Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string ImageURL { get; set; }
+    // public HashSet<ProductAttributeBO> Attributes { get; set; }
+    public SupplierResponseDTO Supplier { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
