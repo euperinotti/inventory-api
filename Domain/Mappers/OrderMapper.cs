24307ea140d698576f7af9e96f5d41c26892442c
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-02-23 00:14:57 -0300

    feat: add order mapper

---
+++ Domain/Mappers/OrderMapper.cs
@@ -0,0 +1,26 @@
+﻿using inventory_api.Domain.Dto.Request;
+using inventory_api.Domain.Entities;
+
+namespace inventory_api.Domain.Mappers;
+
+public static class OrderMapper
+{
+    public static OrderRequestDTO ToDTO(OrderBO bo)
+    {
+        OrderRequestDTO dto = new OrderRequestDTO();
+        dto.Id = bo.Id;
+        dto.Date = bo.Date;
+        dto.Status = bo.Status;
+        dto.Total = bo.Total;
+        dto.SupplierId = (long) bo.Supplier.Id;
+        dto.CreatedAt = bo.CreatedAt;
+        dto.UpdatedAt = bo.UpdatedAt;
+
+        return dto;
+    }
+
+    public static OrderBO ToBO(OrderRequestDTO dto)
+    {
+        return new OrderBO(dto.Id, dto.Date, dto.Status, dto.Total, null, dto.CreatedAt, dto.UpdatedAt);
+    }
+}
