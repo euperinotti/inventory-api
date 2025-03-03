ab8a01479a6f10e4e66c77c7990bb51c85c2e997
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-02 23:04:54 -0300

    feat: add create product usecase

---
+++ Domain/UseCases/Product/CreateProduct.cs
@@ -0,0 +1,33 @@
+﻿using InventoryApi.Domain.Dto.Request;
+using InventoryApi.Domain.Dto.Response;
+using InventoryApi.Domain.Entities;
+using InventoryApi.Domain.Mappers;
+using InventoryApi.Domain.Repository;
+
+namespace InventoryApi.Domain.UseCases.Product;
+
+public class CreateProduct
+{
+    private readonly IProductRepository _repository;
+    private readonly ISupplierRepository _supplierRepository;
+
+    public CreateProduct(IProductRepository repository, ISupplierRepository supplierRepository)
+    {
+        _repository = repository;
+        _supplierRepository = supplierRepository;
+    }
+
+    public ProductResponseDTO Execute(ProductRequestDTO product)
+    {
+        ProductBO bo = ProductMapper.ToBO(product);
+
+        ProductBO created = _repository.Create(bo);
+
+        return ProductMapper.ToDTO(created);
+    }
+
+    public void Validate(ProductRequestDTO product)
+    {
+        SupplierBO supplierBo = _supplierRepository.FindById(product.SupplierId);
+    }
+}
