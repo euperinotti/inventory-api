d8a058840e913d237b7529eb120b5e4a0bf1ee0b
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-03 23:25:46 -0300

    feat: add find supplier usecase

---
+++ Domain/UseCases/Supplier/FindSupplier.cs
@@ -0,0 +1,27 @@
+using InventoryApi.Domain.Assertions;
+using InventoryApi.Domain.Entities;
+using InventoryApi.Domain.Mappers;
+using InventoryApi.Domain.Repository;
+using InventoryApi.Presentation.Dtos.Supplier;
+
+namespace InventoryApi.Domain.UseCases.Supplier;
+
+public class FindSupplier
+{
+    private readonly ISupplierRepository _repository;
+
+    public FindSupplier(ISupplierRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public SupplierResponseDTO Execute(long supplierId)
+    {
+        SupplierBO? bo = _repository.FindById(supplierId);
+
+        Assert.IsNull(bo, "Supplier not found");
+
+        return SupplierMapper.ToDTO(bo);
+    }
+
+}
