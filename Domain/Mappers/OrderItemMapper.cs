423ef869b1b2503291b3dd5002bdaefd69a4192d
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-05 12:44:33 -0300

    feat: add order item mapper

---
+++ Domain/Mappers/OrderItemMapper.cs
@@ -0,0 +1,25 @@
+﻿using InventoryApi.Domain.Entities;
+using InventoryApi.Domain.Dto.Request;
+using InventoryApi.Domain.Dto.Response;
+
+namespace InventoryApi.Domain.Mappers;
+
+public static class OrderItemMapper
+{
+    public static OrderItemResponseDTO ToDTO(OrderItemBO bo)
+    {
+        OrderItemResponseDTO dto = new OrderItemResponseDTO();
+        dto.Id = bo.Id;
+        dto.ProductId = (long) bo.Product.Id;
+        dto.OrderId = (long) bo.Order.Id;
+        dto.Quantity = bo.Quantity;
+        dto.UnitPrice = bo.UnitPrice;
+
+        return dto;
+    }
+
+    public static OrderItemBO ToBO(OrderItemRequestDTO dto)
+    {
+        return new OrderItemBO(dto.Id, null, null, dto.Quantity, dto.UnitPrice);
+    }
+}
