a927886a3f5bda7467dc5924ba57a320ebb63f7b
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-04 23:59:50 -0300

    feat: add order response dto

---
+++ Domain/Dto/Response/OrderResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryApi.Domain.Dto.Response;
+
+public record OrderResponseDTO
+{
+    public long? Id { get; set; }
+    public DateTime Date { get; set; }
+    public OrderStatus Status { get; set; }
+    public long SupplierId { get; set; }
+    public decimal Total { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
