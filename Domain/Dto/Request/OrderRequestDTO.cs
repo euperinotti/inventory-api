d4657c06f82a172325357fb0e57def21ddd98676
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-04 23:55:11 -0300

    feat: add items to order request dto

--- Domain/Dto/Request/OrderRequestDTO.cs
+++ Domain/Dto/Request/OrderRequestDTO.cs
@@ -9,4 +9,5 @@ public record OrderRequestDTO
     public decimal Total { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+    public List<OrderItemRequestDTO> Items { get; set; }
 }
