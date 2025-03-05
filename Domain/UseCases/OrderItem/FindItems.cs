d170efd1182e8a1bc097acaca8239eef92fc56e5
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-05 13:01:09 -0300

    feat: add find items usecase to order item

---
+++ Domain/UseCases/OrderItem/FindItems.cs
@@ -0,0 +1,29 @@
+using InventoryApi.Domain.Dto.Response;
+using InventoryApi.Domain.Entities;
+using InventoryApi.Domain.Mappers;
+using InventoryApi.Domain.Repository;
+using InventoryApi.Domain.UseCases.Order;
+
+namespace InventoryApi.Domain.UseCases.OrderItem;
+
+public class FindItems
+{
+    private readonly IOrderItemRepository _repository;
+    private readonly IOrderRepository _orderRepository;
+
+    public FindItems(IOrderItemRepository repository, IOrderRepository orderRepository)
+    {
+        _repository = repository;
+        _orderRepository = orderRepository;
+    }
+
+    public List<OrderItemResponseDTO> Execute(long orderId)
+    {
+        FindOrder findOrder = new FindOrder(_orderRepository);
+        findOrder.Execute(orderId);
+
+        List<OrderItemBO> items = _repository.FindByOrderId(orderId);
+
+        return items.Select(OrderItemMapper.ToDTO).ToList();
+    }
+}
