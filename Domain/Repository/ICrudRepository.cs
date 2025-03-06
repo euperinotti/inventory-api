203f4bbdf6bfda96229131b9421b0539d35c8e8c
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-06 19:53:15 -0300

    refactor: changed method signature

--- Domain/Repository/ICrudRepository.cs
+++ Domain/Repository/ICrudRepository.cs
@@ -4,7 +4,7 @@ public interface ICrudRepository<TEntity, TKey>
   IEnumerable<TEntity> FindAll();
   TEntity? FindById(TKey id);
   TEntity Create(TEntity supplier);
-  TEntity? Update(TEntity supplier);
+  TEntity Update(TEntity supplier);
   void Delete(TEntity supplier);
   void Delete(TKey? id);
 }
