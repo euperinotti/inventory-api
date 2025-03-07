58cf5d9f4df0957a5edba69c60979ca89923c83d
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-07 13:23:25 -0300

    feat: add user dtos

---
+++ Api/Application/Dto/Response/UserResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace InventoryApi.Domain.Dto.Response;
+
+public class UserResponseDTO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
