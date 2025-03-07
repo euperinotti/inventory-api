48564f3a69f10d8fb915747b335718b6aa1ab116
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-07 13:28:20 -0300

    feat: add find user use case

---
+++ Api/Domain/UseCases/User/FindUser.cs
@@ -0,0 +1,42 @@
+using InventoryApi.Application.Dto.Request;
+using InventoryApi.Application.Dto.Response;
+using InventoryApi.Domain.Assertions;
+using InventoryApi.Domain.Entities;
+using InventoryApi.Domain.Mappers;
+using InventoryApi.Domain.Repository;
+
+namespace InventoryApi.Domain.UseCases.User;
+
+public class FindUser
+{
+    private readonly IUserRepository _repository;
+
+    public FindUser(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<UserResponseDTO> Execute()
+    {
+        IEnumerable<UserBO> users = _repository.FindAll();
+
+        return users.Select(UserMapper.ToDTO).ToList();
+    }
+
+    public UserResponseDTO ExecuteById(UserRequestDTO dto)
+    {
+        UserBO? bo = _repository.FindById((long) dto.Id);
+        Assert.IsNotNull(bo, "User not found");
+
+        return UserMapper.ToDTO(bo);
+    }
+
+    public UserResponseDTO ExecuteByEmail(UserRequestDTO dto)
+    {
+        UserBO? bo = _repository.FindByEmail(dto.Email);
+        Assert.IsNotNull(bo, "User not found");
+
+        return UserMapper.ToDTO(bo);
+    }
+
+}
