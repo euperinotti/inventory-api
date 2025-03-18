7f3f27a3632679f437b98fc436e3d293cd1753ee
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-17 21:51:32 -0300

    fix: assertions readability

--- Api/Domain/Assertions/Assert.cs
+++ Api/Domain/Assertions/Assert.cs
@@ -4,145 +4,181 @@ public static class Assert
 {
     public static void IsNull(object? value, string message)
     {
-        if (value != null)
+        if (value is null)
         {
-            throw new ArgumentNullException(message);
+            return;
         }
+
+        throw new ArgumentNullException(message);
     }
 
-    public static void IsNotNull(object value, string message)
+    public static void IsNotNull(object? value, string message)
     {
-        if (value == null)
+        if (value is not null)
         {
-            throw new ArgumentNullException(message);
+            return;
         }
+
+        throw new ArgumentNullException(message);
     }
 
     public static void IsTrue(bool condition, string message)
     {
-        if (!condition)
+        if (condition)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsFalse(bool condition, string message)
     {
-        if (condition)
+        if (!condition)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsNullOrEmpty(string value, string message)
     {
         if (string.IsNullOrEmpty(value))
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsNullOrWhiteSpace(string value, string message)
     {
         if (string.IsNullOrWhiteSpace(value))
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsGreaterThan<T>(T value, T limit, string message) where T : IComparable<T>
     {
         if (value.CompareTo(limit) > 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsLessThan<T>(T value, T limit, string message) where T : IComparable<T>
     {
         if (value.CompareTo(limit) < 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsGreaterThanOrEqual<T>(T value, T limit, string message) where T : IComparable<T>
     {
-        if (value.CompareTo(limit) < 0)
+        if (value.CompareTo(limit) >= 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsLessThanOrEqual<T>(T value, T limit, string message) where T : IComparable<T>
     {
         if (value.CompareTo(limit) <= 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsInRange<T>(T value, T min, T max, string message) where T : IComparable<T>
     {
         if (value.CompareTo(min) < 0 || value.CompareTo(max) > 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsEqual<T>(T value1, T value2, string message) where T : IComparable<T>
     {
-        if (value1.CompareTo(value2) != 0)
+        if (value1.CompareTo(value2) == 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsNotEqual<T>(T value1, T value2, string message) where T : IComparable<T>
     {
-        if (value1.CompareTo(value2) == 0)
+        if (value1.CompareTo(value2) != 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsIn<T>(T value, params T[] values) where T : IComparable<T>
     {
-        if (!values.Contains(value))
+        if (values.Contains(value))
         {
-            throw new ArgumentException($"'{value}' is not in '{string.Join(",", values)}'");
+            return;
         }
+
+        throw new ArgumentException($"'{value}' is not in '{string.Join(",", values)}'");
     }
 
     public static void IsNotIn<T>(T value, params T[] values) where T : IComparable<T>
     {
-        if (values.Contains(value))
+        if (!values.Contains(value))
         {
-            throw new ArgumentException($"'{value}' is in '{string.Join(",", values)}'");
+            return;
         }
+
+        throw new ArgumentException($"'{value}' is in '{string.Join(",", values)}'");
     }
 
     public static void IsBetween<T>(T value, T min, T max, string message) where T : IComparable<T>
     {
         if (value.CompareTo(min) < 0 || value.CompareTo(max) > 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsNotBetween<T>(T value, T min, T max, string message) where T : IComparable<T>
     {
         if (value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0)
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 
     public static void IsHttpUrl(string url, string message)
     {
-        if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+        if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
         {
-            throw new ArgumentException(message);
+            return;
         }
+
+        throw new ArgumentException(message);
     }
 }
