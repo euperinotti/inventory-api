9b1ee5e4976e30e3698b5df65afb526794630e51
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-17 21:08:57 -0300

    feat: add update method to product

--- Api/Domain/Entities/ProductBO.cs
+++ Api/Domain/Entities/ProductBO.cs
@@ -9,7 +9,6 @@ public class ProductBO
     public string Description { get; private set; }
     public decimal Price { get; private set; }
     public int Quantity { get; private set; }
-
     public string ImageURL { get; private set; }
 
     // public HashSet<ProductAttributeBO> Attributes { get; private set; }
@@ -29,11 +28,23 @@ public class ProductBO
         Validate();
     }
 
-    public void Validate()
+    private void Validate()
     {
         Assert.IsNullOrWhiteSpace(Name, "Product name is required");
         Assert.IsGreaterThan(Price, 0, "Product price must be greater than zero");
         Assert.IsGreaterThanOrEqual(Quantity, 0, "Product quantity must be greater than or equal to zero");
         Assert.IsHttpUrl(ImageURL, "Product image URL is invalid");
+    }
+
+    public void UpdateProduct(ProductBO bo)
+    {
+        Name = bo.Name;
+        Description = bo.Description;
+        Price = bo.Price;
+        Quantity = bo.Quantity;
+        ImageURL = bo.ImageURL;
+        Supplier = bo.Supplier;
+
+        Validate();
     }
 }
