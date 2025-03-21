95e2b20dfab8de756f2fc3088801ad5d26a945ab
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-20 23:20:25 -0300

    feat: add product attribute factory

---
+++ Api/Domain/Factories/ProductAttributeFactory.cs
@@ -0,0 +1,19 @@
+﻿using InventoryApi.Domain.Entities;
+
+namespace InventoryApi.Domain.Factories;
+
+public class ProductAttributeFactory
+{
+    // TODO: Make this generic and solve
+    public static ProductAttributeBO<object> Create<T>(long? id, string key, T value, AttributeDataType dataType)
+    {
+        return dataType switch
+        {
+            AttributeDataType.String => new ProductAttributeBO<string>(id, key, (string)value, dataType),
+            AttributeDataType.Integer => new ProductAttributeBO<int>(id, key, Convert.ToInt32(value), dataType),
+            AttributeDataType.Decimal => new ProductAttributeBO<decimal>(id, key, Convert.ToDecimal(value), dataType),
+            AttributeDataType.Boolean => new ProductAttributeBO<bool>(id, key, Convert.ToBoolean(value), dataType),
+            _ => throw new ArgumentException("Unsupported data type")
+        };
+    }
+}
