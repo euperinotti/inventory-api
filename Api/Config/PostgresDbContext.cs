105a18008a0bb9deb55ccf52551c26630f3fe731
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-21 21:48:44 -0300

    feat: add PostgresDbContext.cs

---
+++ Api/Config/PostgresDbContext.cs
@@ -0,0 +1,11 @@
+﻿using InventoryApi.Infra.Pgsql.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApi.Config;
+
+public class PostgresDbContext : DbContext
+{
+    public PostgresDbContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<PgSupplierEntity> Suppliers { get; set; }
+}
