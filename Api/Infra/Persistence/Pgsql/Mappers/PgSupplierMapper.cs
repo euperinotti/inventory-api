537287e0cf3351fb9988bee5a3c8975cabc6b90e
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-21 21:49:40 -0300

    feat: add pg supplier mapper

---
+++ Api/Infra/Persistence/Pgsql/Mappers/PgSupplierMapper.cs
@@ -0,0 +1,27 @@
+﻿using InventoryApi.Domain.Dto.Request;
+using InventoryApi.Domain.Entities;
+using InventoryApi.Infra.Pgsql.Entities;
+
+namespace InventoryApi.Infra.Persistence.Pgsql.Mappers;
+
+public static class PgSupplierMapper
+{
+    public static PgSupplierEntity ToEntity(SupplierBO bo)
+    {
+        PgSupplierEntity entity = new PgSupplierEntity();
+        entity.Id = bo.Id;
+        entity.CreatedAt = bo.CreatedAt;
+        entity.UpdatedAt = bo.UpdatedAt;
+        entity.Name = bo.Name;
+        entity.Cnpj = bo.Cnpj;
+        entity.Address = bo.Address;
+        entity.Contact = bo.Contact;
+
+        return entity;
+    }
+
+    public static SupplierBO ToBO(PgSupplierEntity entity)
+    {
+        return new SupplierBO(entity.Id, entity.Name, entity.Cnpj, entity.Address, entity.Contact, entity.CreatedAt, entity.UpdatedAt);
+    }
+}
