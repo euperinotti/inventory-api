e6f328b558ce046de8252c73eb965bb454f4de21
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-21 21:10:21 -0300

    feat: add supplier service

---
+++ Api/Application/Services/SupplierService.cs
@@ -0,0 +1,30 @@
+﻿using InventoryApi.Domain.Dto;
+using InventoryApi.Domain.Dto.Request;
+using InventoryApi.Domain.Repository;
+using InventoryApi.Domain.UseCases.Supplier;
+
+namespace InventoryApi.Application.Services;
+
+public class SupplierService
+{
+    private readonly ISupplierRepository _supplierRepository;
+
+    public SupplierService(ISupplierRepository supplierRepository)
+    {
+        _supplierRepository = supplierRepository;
+    }
+
+    public SupplierDTO Update(SupplierDTO dto)
+    {
+        UpdateSupplier usecase = new UpdateSupplier(_supplierRepository);
+
+        return usecase.Execute(dto);
+    }
+
+    public List<SupplierDTO> FindAll()
+    {
+        FindSupplier usecase = new FindSupplier(_supplierRepository);
+
+        return usecase.Execute();
+    }
+}
