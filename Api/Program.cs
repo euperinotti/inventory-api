179915062948681aede800824b98a7b5b62fb223
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-03-21 21:51:21 -0300

    feat: add db context to Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,16 @@
+using InventoryApi.Config;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PostgresDbContext>();
+builder.Services.AddDbContext<PostgresDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
