9cfbe389bdfd77c7ffd97833f385fecd0cd46600
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-04-22 23:01:43 -0300

    feat: add authentication middleware

---
+++ Api/Config/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.Config.Middlewares;
+
+// TODO: Revisar
+public class AuthenticationMiddleware
+{
+    public void Configure(IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "your_issuer",
+                    ValidAudience = "your_audience",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key"))
+                };
+            });
+    }
+}
