4ac2c1dd570d9acd47464240298c849288a8b2e0
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-04-29 00:22:49 -0300

    test: add tests for supplier bo

---
+++ Tests/Unit/Domain/Entities/SupplierBOTests.cs
@@ -0,0 +1,70 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Exceptions;
+
+namespace Tests.Unit.Domain.Entities;
+
+public class SupplierBOTests
+{
+    [Test]
+    public void Constructor_ShouldThrowIfNameIsNull()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, null, "100", "Address", "Contact", DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfNameIsWhiteSpace()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "    ", "100", "Address", "Contact", DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfCnpjIsNull()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "Eric Kong", null, "Address", "Contact", DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfCnpjIsWhiteSpace()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "Eric Kong", "", "Address", "Contact", DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfContactIsNull()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "Eric Kong", "61264680887", "Address", null, DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfContactIsWhiteSpace()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "Eric Kong", "61264680887", "Address", "     ", DateTime.Now, DateTime.Now);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrowIfCnpjIsInvalid()
+    {
+        Assert.Throws<AssertException>(() =>
+        {
+            new SupplierBO(1, "Eric Kong", "61264680887", "Address", "     ", DateTime.Now, DateTime.Now);
+        });
+    }
+}
