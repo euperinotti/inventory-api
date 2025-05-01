3fdeab372255b03722911cf90177a0c765872e82
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-04-30 22:07:51 -0300

    refactor: throwing validation exception

--- Api/Infra/Validators/PasswordValidator.cs
+++ Api/Infra/Validators/PasswordValidator.cs
@@ -1,4 +1,5 @@
 using Api.Domain.Assertions;
+using Api.Domain.Exceptions;
 using Api.Domain.Validators;
 using Microsoft.AspNetCore.Identity;
 
@@ -6,24 +7,28 @@ namespace Api.Infra.Validators;
 
 public class PasswordValidator : IPasswordValidator
 {
-  public void Validate(string password)
-  {
-    Assert.IsGreaterThanOrEqual(password.Length, 8, "Password must be at least 8 characters long");
+    public void Validate(string password)
+    {
+        Assert.IsGreaterThanOrEqual(password.Length, 8, "Password must be at least 8 characters long");
 
-    if (!password.Any(char.IsUpper)) {
-      throw new ArgumentException("Password must contain at least one uppercase letter");
-    }
+        if (!password.Any(char.IsUpper))
+        {
+            throw new ValidationException("Password must contain at least one uppercase letter");
+        }
 
-    if (!password.Any(char.IsLower)) {
-      throw new ArgumentException("Password must contain at least one lowercase letter");
-    }
+        if (!password.Any(char.IsLower))
+        {
+            throw new ValidationException("Password must contain at least one lowercase letter");
+        }
 
-    if (!password.Any(char.IsDigit)) {
-      throw new ArgumentException("Password must contain at least one digit");
-    }
+        if (!password.Any(char.IsDigit))
+        {
+            throw new ValidationException("Password must contain at least one digit");
+        }
 
-    if (!password.Any(ch => !char.IsLetterOrDigit(ch) && !char.IsWhiteSpace(ch))) {
-      throw new ArgumentException("Password must contain at least one special character");
+        if (!password.Any(ch => !char.IsLetterOrDigit(ch) && !char.IsWhiteSpace(ch)))
+        {
+            throw new ValidationException("Password must contain at least one special character");
+        }
     }
-  }
 }
