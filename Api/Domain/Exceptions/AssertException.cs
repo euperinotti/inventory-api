5e9aa2cc5165b2d43144f3497f3d2daafd773748
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-04-30 21:38:59 -0300

    feat: add assert exception messages

--- Api/Domain/Exceptions/AssertException.cs
+++ Api/Domain/Exceptions/AssertException.cs
@@ -1,3 +1,9 @@
 namespace Api.Domain.Exceptions;
 
 public class AssertException(string message) : Exception(message);
+
+public static class AssertExceptionMessage
+{
+    public static readonly string INVALID_CNPJ = "Invalid Cnpj";
+    public static readonly string INVALID_CNPJ_LENGTH = "Invalid Cnpj length";
+}
