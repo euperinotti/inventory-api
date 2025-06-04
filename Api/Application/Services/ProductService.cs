555b205e24b3cf91c500149ff4a6710b6ca1ac3f
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-06-04 06:42:44 -0300

    feat: add product service

---
+++ Api/Application/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using Api.Domain.Dto.Request;
+using Api.Domain.Repository;
+using Api.Domain.UseCases.Product;
+
+namespace Api.Application.Services;
+
+public class ProductService
+{
+    private readonly IProductRepository _repository;
+    private readonly ISupplierRepository _supplierRepository;
+
+    public ProductService(IProductRepository repository, ISupplierRepository supplierRepository)
+    {
+        _repository = repository;
+        _supplierRepository = supplierRepository;
+    }
+
+    public List<ProductDTO> FindAll()
+    {
+        FindProduct usecase = new FindProduct(_repository);
+
+        return usecase.Execute();
+    }
+
+    public ProductDTO FindById(long id)
+    {
+        FindProduct usecase = new FindProduct(_repository);
+
+        return usecase.Execute(id);
+    }
+
+    public ProductDTO Create(ProductDTO dto)
+    {
+        CreateProduct usecase = new CreateProduct(_repository, _supplierRepository);
+
+        return usecase.Execute(dto);
+    }
+
+    public ProductDTO Update(ProductDTO dto)
+    {
+        UpdateProduct usecase = new UpdateProduct(_repository, _supplierRepository);
+
+        return usecase.Execute(dto);
+    }
+
+    public void Delete(long id)
+    {
+        DeleteProduct usecase = new DeleteProduct(_repository);
+
+        usecase.Execute(id);
+    }
+}
