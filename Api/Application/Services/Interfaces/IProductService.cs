1692de0c1faf2d0c4568439c046030cdd5fb69f7
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-06-07 17:06:36 -0300

    feat: add services interface

---
+++ Api/Application/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Api.Domain.Dto.Request;
+
+namespace Api.Application.Services.Interfaces;
+
+public interface IProductService
+{
+    List<ProductDTO> FindAll();
+    ProductDTO FindById(long id);
+    ProductDTO Create(ProductDTO dto);
+    ProductDTO Update(ProductDTO dto);
+    void Delete(long id);
+}
---
+++ Api/Application/Services/Interfaces/ISupplierService.cs
@@ -0,0 +1,12 @@
+﻿using Api.Domain.Dto;
+
+namespace Api.Application.Services.Interfaces;
+
+public interface ISupplierService
+{
+    List<SupplierDTO> FindAll();
+    SupplierDTO FindById(long id);
+    SupplierDTO Create(SupplierDTO dto);
+    SupplierDTO Update(SupplierDTO dto);
+    void Delete(long id);
+}
