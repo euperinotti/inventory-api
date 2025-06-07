8b5293133603bbb90e38cb3c6c6f5ba628361599
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-06-07 17:09:54 -0300

    chore: configuring dependency injection

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,9 @@
+using Api.Application.Services.Implementation;
+using Api.Application.Services.Interfaces;
 using Api.Config.Middlewares;
+using Api.Domain.Repository;
 using Api.Infra.Persistence.Pgsql.Config;
+using Api.Infra.Persistence.Pgsql.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,10 @@ builder.Services.AddDbContext<PostgresDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<ISupplierRepository, PgSupplierRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, PgProductRepository>();
 
 AuthenticationMiddleware authenticationMiddleware = new AuthenticationMiddleware();
 authenticationMiddleware.Configure(builder.Services, builder.Configuration);
