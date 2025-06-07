d1e3e1b719bd341323a923fb5765d0c5a87b9523
Guilherme Perinotti <guilherme9115@hotmail.com>
2025-06-07 17:07:55 -0300

    feat: add pg product mapper

---
+++ Api/Infra/Persistence/Pgsql/Mappers/PgProductMapper.cs
@@ -0,0 +1,20 @@
+﻿using Api.Domain.Entities;
+using Api.Infra.Persistence.Pgsql.Entities;
+
+namespace Api.Infra.Persistence.Pgsql.Mappers;
+
+public static class PgProductMapper
+{
+    public static PgProductEntity ToEntity(ProductBO bo)
+    {
+        return new PgProductEntity(bo.Id, bo.Name, bo.Description, bo.Price, bo.Quantity, bo.ImageURL,
+            PgSupplierMapper.ToEntity(bo.Supplier),
+            bo.CreatedAt, bo.UpdatedAt);
+    }
+
+    public static ProductBO ToBO(PgProductEntity entity)
+    {
+        return new ProductBO(entity.Id, entity.Name, entity.Description, entity.Price, entity.Quantity, entity.ImageURL,
+            PgSupplierMapper.ToBO(entity.Supplier));
+    }
+}
